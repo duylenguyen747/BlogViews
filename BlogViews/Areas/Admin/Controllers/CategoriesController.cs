0db6f3743b9d17f81f50c3229ad57d08182f3a32
Nguyen Le Duy <duy.nguyen.le@datvietvac.vn>
2023-06-16 17:34:19 +0700

    3

--- BlogViews/Areas/Admin/Controllers/CategoriesController.cs
+++ BlogViews/Areas/Admin/Controllers/CategoriesController.cs
@@ -6,166 +6,158 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using BlogViews.Models;
-using BlogViews.Helpper;
-using PagedList.Core;
 
 namespace BlogViews.Areas.Admin.Controllers
 {
-	[Area("Admin")]
-	public class CategoriesController : Controller
-	{
-		private readonly BlogViewsDbContext _context;
+    [Area("Admin")]
+    public class CategoriesController : Controller
+    {
+        private readonly BlogViewsDbContext _context;
 
-		public CategoriesController(BlogViewsDbContext context)
-		{
-			_context = context;
-		}
+        public CategoriesController(BlogViewsDbContext context)
+        {
+            _context = context;
+        }
 
-		// GET: Admin/Categories
-		public async Task<IActionResult> Index(int? page)
-		{
-			var pageNumber = page == null || page <= 0 ? 1 : page.Value;
-			var pageSize = 1;
-			var lsCategories = _context.Categories
-				.OrderByDescending(x => x.CatId);
+        // GET: Admin/Categories
+        public async Task<IActionResult> Index()
+        {
+              return _context.Categories != null ? 
+                          View(await _context.Categories.ToListAsync()) :
+                          Problem("Entity set 'BlogViewsDbContext.Categories'  is null.");
+        }
 
-			PagedList<Category> models = new PagedList<Category>(lsCategories, pageNumber, pageSize);
+        // GET: Admin/Categories/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Categories == null)
+            {
+                return NotFound();
+            }
 
-			ViewBag.CurrentPage = pageNumber;
-			return View(models);
-		}
+            var category = await _context.Categories
+                .FirstOrDefaultAsync(m => m.CatId == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
 
-		// GET: Admin/Categories/Details/5
-		public async Task<IActionResult> Details(int? id)
-		{
-			if (id == null || _context.Categories == null)
-			{
-				return NotFound();
-			}
+            return View(category);
+        }
 
-			var category = await _context.Categories
-				.FirstOrDefaultAsync(m => m.CatId == id);
-			if (category == null)
-			{
-				return NotFound();
-			}
+        // GET: Admin/Categories/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
 
-			return View(category);
-		}
+        // POST: Admin/Categories/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("CatId,CatName,Title,Alias,MetaDesc,MetaKey,Thumb,Published,Ordering,Parents,Levels,Icon,Cover,Description")] Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(category);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
 
-		// GET: Admin/Categories/Create
-		public IActionResult Create()
-		{
-			return View();
-		}
+        // GET: Admin/Categories/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Categories == null)
+            {
+                return NotFound();
+            }
 
-		// POST: Admin/Categories/Create
-		// To protect from overposting attacks, enable the specific properties you want to bind to.
-		// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-		[HttpPost]
-		[ValidateAntiForgeryToken]
-		public async Task<IActionResult> Create([Bind("CatId,CatName,Title,Alias,MetaDesc,MetaKey,Thumb,Publisher,Ordering,Parents,Levels,Icon,Cover,Description")] Category category)
-		{
-			if (ModelState.IsValid)
-			{
-				_context.Add(category);
-				await _context.SaveChangesAsync();
-				return RedirectToAction(nameof(Index));
-			}
-			return View(category);
-		}
+            var category = await _context.Categories.FindAsync(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
 
-		// GET: Admin/Categories/Edit/5
-		public async Task<IActionResult> Edit(int? id)
-		{
-			if (id == null || _context.Categories == null)
-			{
-				return NotFound();
-			}
+        // POST: Admin/Categories/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("CatId,CatName,Title,Alias,MetaDesc,MetaKey,Thumb,Published,Ordering,Parents,Levels,Icon,Cover,Description")] Category category)
+        {
+            if (id != category.CatId)
+            {
+                return NotFound();
+            }
 
-			var category = await _context.Categories.FindAsync(id);
-			if (category == null)
-			{
-				return NotFound();
-			}
-			return View(category);
-		}
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(category);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CategoryExists(category.CatId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
 
-		// POST: Admin/Categories/Edit/5
-		// To protect from overposting attacks, enable the specific properties you want to bind to.
-		// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-		[HttpPost]
-		[ValidateAntiForgeryToken]
-		public async Task<IActionResult> Edit(int id, [Bind("CatId,CatName,Title,Alias,MetaDesc,MetaKey,Thumb,Publisher,Ordering,Parents,Levels,Icon,Cover,Description")] Category category)
-		{
-			if (id != category.CatId)
-			{
-				return NotFound();
-			}
+        // GET: Admin/Categories/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Categories == null)
+            {
+                return NotFound();
+            }
 
-			if (ModelState.IsValid)
-			{
-				try
-				{
-					_context.Update(category);
-					await _context.SaveChangesAsync();
-				}
-				catch (DbUpdateConcurrencyException)
-				{
-					if (!CategoryExists(category.CatId))
-					{
-						return NotFound();
-					}
-					else
-					{
-						throw;
-					}
-				}
-				return RedirectToAction(nameof(Index));
-			}
-			return View(category);
-		}
+            var category = await _context.Categories
+                .FirstOrDefaultAsync(m => m.CatId == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
 
-		// GET: Admin/Categories/Delete/5
-		public async Task<IActionResult> Delete(int? id)
-		{
-			if (id == null || _context.Categories == null)
-			{
-				return NotFound();
-			}
+            return View(category);
+        }
 
-			var category = await _context.Categories
-				.FirstOrDefaultAsync(m => m.CatId == id);
-			if (category == null)
-			{
-				return NotFound();
-			}
+        // POST: Admin/Categories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Categories == null)
+            {
+                return Problem("Entity set 'BlogViewsDbContext.Categories'  is null.");
+            }
+            var category = await _context.Categories.FindAsync(id);
+            if (category != null)
+            {
+                _context.Categories.Remove(category);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
 
-			return View(category);
-		}
-
-		// POST: Admin/Categories/Delete/5
-		[HttpPost, ActionName("Delete")]
-		[ValidateAntiForgeryToken]
-		public async Task<IActionResult> DeleteConfirmed(int id)
-		{
-			if (_context.Categories == null)
-			{
-				return Problem("Entity set 'BlogViewsDbContext.Categories'  is null.");
-			}
-			var category = await _context.Categories.FindAsync(id);
-			if (category != null)
-			{
-				_context.Categories.Remove(category);
-			}
-
-			await _context.SaveChangesAsync();
-			return RedirectToAction(nameof(Index));
-		}
-
-		private bool CategoryExists(int id)
-		{
-			return (_context.Categories?.Any(e => e.CatId == id)).GetValueOrDefault();
-		}
-	}
+        private bool CategoryExists(int id)
+        {
+          return (_context.Categories?.Any(e => e.CatId == id)).GetValueOrDefault();
+        }
+    }
 }
--- BlogViews/Models/Category.cs
+++ BlogViews/Models/Category.cs
@@ -5,33 +5,33 @@ namespace BlogViews.Models;
 
 public partial class Category
 {
-    public int CatId { get; set; }
+	public int CatId { get; set; }
 
-    public string? CatName { get; set; }
+	public string? CatName { get; set; }
 
-    public string? Title { get; set; }
+	public string? Title { get; set; }
 
-    public string? Alias { get; set; }
+	public string? Alias { get; set; }
 
-    public string? MetaDesc { get; set; }
+	public string? MetaDesc { get; set; }
 
-    public string? MetaKey { get; set; }
+	public string? MetaKey { get; set; }
 
-    public string? Thumb { get; set; }
+	public string? Thumb { get; set; }
 
-    public bool? Publisher { get; set; }
+	public bool Published { get; set; }
 
-    public int? Ordering { get; set; }
+	public int? Ordering { get; set; }
 
-    public int? Parents { get; set; }
+	public int? Parents { get; set; }
 
-    public int? Levels { get; set; }
+	public int? Levels { get; set; }
 
-    public string Icon { get; set; } = null!;
+	public string Icon { get; set; } = null!;
 
-    public string? Cover { get; set; }
+	public string? Cover { get; set; }
 
-    public string? Description { get; set; }
+	public string? Description { get; set; }
 
-    public virtual ICollection<Post> Posts { get; set; } = new List<Post>();
+	public virtual ICollection<Post> Posts { get; set; } = new List<Post>();
 }
